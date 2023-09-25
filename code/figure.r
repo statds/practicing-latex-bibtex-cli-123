5b4cc7eaa4f7dd304def908d9b41901ff5ff2acc
cli-123 <carol.li@uconn.edu>
2023-09-25 16:00:43 -0400

    updated table code

--- code/figure.r
+++ code/figure.r
@@ -1,11 +1,11 @@
 pdf("plot.pdf", pointsize = 8, height = 4, width = 6)
-plot(x = 1:10,                        
-     y = 1:10,                        
-     type = "p",                      
+plot(x = 1:10,
+     y = 1:10,
+     type = "p",
      main = "My First Plot",
      xlab = "x",
      ylab = "yl",
-     xlim = c(0, 21),                  
-     ylim = c(0, 21),                                       
-     cex = 1)  
+     xlim = c(0, 21),
+     ylim = c(0, 21),
+     cex = 1)
 dev.off()
