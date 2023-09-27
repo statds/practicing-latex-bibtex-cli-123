540349e764d8d95d01fc05df2bf4645a48b0e8b0
cli-123 <carol.li@uconn.edu>
2023-09-27 14:32:31 -0400

    updated conversion to pdf using pdflatex and bibtex

--- code/figure.r
+++ code/figure.r
@@ -1,11 +1,7 @@
-pdf("plot.pdf", pointsize = 8, height = 4, width = 6)
-plot(x = 1:10,
-     y = 1:10,
-     type = "p",
-     main = "My First Plot",
-     xlab = "x",
-     ylab = "yl",
-     xlim = c(0, 21),
-     ylim = c(0, 21),
-     cex = 1)
+pdf("mygraph.pdf")
+attach(mtcars)
+plot(wt, mpg)
+abline(lm(mpg~wt))
+title("Regression of MPG on Weight")
+detach(mtcars)
 dev.off()
